aa1dcb288a28c63bb4258147fb2b3f471a36ce2e
SumiranBhawsar <sumiranbhawsar149@gmail.com>
2025-08-14 21:19:52 +0530

    changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -138,8 +138,9 @@ const loginUser = asyncHandler(async (req, res) => {
 
     const cookieOptions = {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        secure: process.env.NODE_ENV === "production", // true on Render
+        sameSite: "none", // required for cross-site cookies
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     };
 
     return res
