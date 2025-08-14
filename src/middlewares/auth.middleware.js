e712138969d2fad1f9f15573a91e5f5309969e08
SumiranBhawsar <sumiranbhawsar149@gmail.com>
2025-08-14 20:55:44 +0530

    changes in auth.middleware.js

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,29 +1,38 @@
-import jwt from "jsonwebtoken";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
+import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
-    const token =
-        req.cookies?.accessToken ||
-        req.header("Authorization")?.replace("Bearer ", "");
-
-    if (!token) {
-        throw new ApiError(401, "Access token missing");
-    }
+    // get jwt token from cookie or header
+    // check if token present or ton
+    // verify token is valid
+    // if valid then find user from database with the help of token data
+    // if user found then attach user to req object
+    // next()
 
     try {
-        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-        req.user = await User.findById(decoded._id).select(
+        const token =
+            req.cookies?.accessToken ||
+            req.header("authorization")?.replace("Bearer ", "");
+
+        if (!token) {
+            throw new ApiError(401, "Unauthorized");
+        }
+
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+        const user = await User.findById(decodedToken?._id).select(
             "-password -refreshToken"
         );
 
-        if (!req.user) {
-            throw new ApiError(401, "Invalid token: user not found");
+        if (!user) {
+            throw new ApiError(401, "Unauthorized");
         }
 
+        req.user = user._id;
         next();
-    } catch (err) {
-        throw new ApiError(401, "Invalid or expired token");
+    } catch (error) {
+        throw new ApiError(401, error?.message || "invalid access token");
     }
 });
